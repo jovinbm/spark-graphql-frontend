e6fb384d04a1e7e99cca01efc3e5e35fec29fdda
Jovin Mwilanga <jovinbeda@gmail.com>
2023-11-27 20:00:23 -0500

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['next/core-web-vitals', 'alloy', 'alloy/react', 'alloy/typescript'],
+  env: {
+    browser: true,
+  },
+  globals: {},
+  rules: {
+    'max-params': 0,
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+};
